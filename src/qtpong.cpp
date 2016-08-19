e97b9e908faf88f65f5d7756c6cde0032844497d
Fran89 <Fran89@users.noreply.github.com>
2016-08-18 22:35:18 -0400

    Set scene rectangle to a rectangle at 0,0,1024,512

--- src/qtpong.cpp
+++ src/qtpong.cpp
@@ -7,6 +7,7 @@ QTPong::QTPong(QWidget *parent) :
 {
     ui->setupUi(this);
     myScene = new QGraphicsScene;
+    myScene->setSceneRect(0,0,1024,512); // This size is an example one for now
 }
 
 QTPong::~QTPong()
